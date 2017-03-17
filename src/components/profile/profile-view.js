3d4d0025797fa3680cad8b532ef10af8e7888469
turbobeast <georgemichaelfaust@gmail.com>
2017-03-17 10:25:21 -0400

    step 1: created profile view container and components

---
+++ src/components/profile/profile-view.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import Profile from './profile'
+
+class ProfileView extends Component {
+  componentWillMount() {
+    this.props.getRobots()
+  }
+
+  render() {
+    const { isPending, robot } = this.props
+    return (
+      <div className="profilePage">
+        {
+          isPending
+          ? <h2>Loading... </h2>
+          : <Profile robot={robot} />
+        }
+      </div>
+    )
+  }
+}
+
+ProfileView.defaultProps = {
+  robot: null,
+}
+
+ProfileView.propTypes = {
+  isPending: React.PropTypes.bool.isRequired,
+  robot: React.PropTypes.object,
+  getRobots: React.PropTypes.func.isRequired,
+}
+
+export default ProfileView
---
+++ src/containers/robot-profile-view.container.js
@@ -0,0 +1,24 @@
+import { connect } from 'react-redux'
+import { getRobots } from '../actions'
+import ProfileView from '../components/profile/profile-view'
+
+const mapStateToProps = (state, ownProps) => {
+  const id = ownProps.id || 1
+  const isPending = state.robotData.isPending
+  let robot = null
+
+  if (!isPending) {
+    robot = state.robotData.robots.find(robo => robo.id === id)
+  }
+
+  return {
+    robot,
+    isPending,
+  }
+}
+
+const mapDispatchToProps = dispatch => ({
+  getRobots: () => dispatch(getRobots()),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProfileView)
