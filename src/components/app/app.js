4a6e59d961ed32f9bc8a843957ce055b9339107e
turbobeast <georgemichaelfaust@gmail.com>
2017-03-17 10:25:21 -0400

    step 1: created profile view container and components

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import './app.css'
-import RobotFilterViewContainer from '../../containers/robot-filter-view.container'
+// import RobotFilterViewContainer from '../../containers/robot-filter-view.container'
+import RobotProfileViewContainer from '../../containers/robot-profile-view.container'
 
 function App() {
   return (
     <div className="tc">
       <h1>RoboDex</h1>
-      <RobotFilterViewContainer />
+      {/* <RobotFilterViewContainer /> */}
+      <RobotProfileViewContainer />
     </div>
   )
 }
