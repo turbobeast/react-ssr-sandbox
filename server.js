6ddef17b394b990c4964357dfb771f266e724748
turbobeast <georgemichaelfaust@gmail.com>
2017-03-17 13:10:45 -0400

    step 2: es6 template literal for index - added manifest plugin

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const path = require('path')
+const manifest = require('./build/manifest.json')
+const { htmlTemplate } = require('./src/index.html.js')
 
 const app = express()
 const port = 8080
 
 app.use('/static', express.static(path.join(__dirname, 'build', 'static')))
 app.use((req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'))
+  res.send(htmlTemplate({ cssPath: manifest['main.css'], jsPath: manifest['main.js'] }))
 })
 
 app.listen(port, () => {
