3f013df99cee49d7ea80bc064cd6a40cfd051a86
turbobeast <georgemichaelfaust@gmail.com>
2017-03-21 11:36:21 -0400

    step 4: added radium - converted stylesto JS

--- src/components/profile/profile-view.js
+++ src/components/profile/profile-view.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import Radium from 'radium'
 import Profile from './profile'
-import './profile.css'
+import { profilePage, button } from './profile.css'
 
 class ProfileView extends Component {
   componentWillMount() {
@@ -11,13 +12,13 @@ class ProfileView extends Component {
   render() {
     const { isPending, robot } = this.props
     return (
-      <div className="profilePage">
+      <div style={profilePage}>
         {
           isPending
           ? <h2>Loading... </h2>
           : <Profile robot={robot} />
         }
-        <Link className="button" to="/">Back</Link>
+        <Link style={button} to="/">Back</Link>
       </div>
     )
   }
@@ -33,4 +34,4 @@ ProfileView.propTypes = {
   getRobots: React.PropTypes.func.isRequired,
 }
 
-export default ProfileView
+export default Radium(ProfileView)
---
+++ src/components/profile/profile.css.js
@@ -0,0 +1,67 @@
+export const profilePage = {
+  maxWidth: '800px',
+  margin: '0px auto',
+  textAlign: 'left',
+}
+
+export const profile = {
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'left',
+  alignItems: 'center',
+  height: '100%',
+}
+
+export const column = {
+  height: '100px',
+  border: 'solid white 2px',
+}
+
+export const headshot = {
+  width: '30vw',
+  height: '30vw',
+  textAlign: 'center',
+  display: 'flex',
+  flexDirection: 'column',
+  background: '#9eebcf',
+  borderRadius: '12px',
+  heading: {
+    margin: 0,
+    flexShrink: 1,
+    fontFamily: 'arial',
+    fontSize: '2vw',
+  },
+}
+
+export const img = {
+  display: 'block',
+  margin: '8% auto',
+  background: 'white',
+  borderRadius: '100%',
+  height: '20vw',
+  flexGrow: 1,
+}
+
+export const address = {
+  width: 'calc(80% - 25vw)',
+  height: '30vw',
+  textAlign: 'left',
+  paddingLeft: '30px',
+  p: {
+    margin: 0,
+  },
+  button: {
+    margin: '20px 0px',
+  },
+}
+
+export const button = {
+  display: 'inline-block',
+  padding: '8px 16px',
+  borderRadius: '6px',
+  fontWeight: 700,
+  color: '#ffffff',
+  backgroundColor: '#131313',
+  textDecoration: 'none',
+  margin: '20px 0px',
+}
