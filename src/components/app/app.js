5529af8ecdd9a8ef3d371c8bd6dbd97280ec861a
turbobeast <georgemichaelfaust@gmail.com>
2017-03-24 14:39:38 -0400

    lazy loading with require.ensure

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,8 +1,16 @@
 import React from 'react'
 import { Route, Switch, Redirect } from 'react-router-dom'
 import './app.css'
+import Lazy from '../lazy'
 import RobotFilterViewContainer from '../../containers/robot-filter-view.container'
-import RobotProfileViewContainer from '../../containers/robot-profile-view.container'
+
+const loadProfile = process.env.NODE_SERVER
+  ? cb => cb(require('../../containers/robot-profile-view.container'))
+  : cb => require.ensure([], (require) => {
+    cb(require('../../containers/robot-profile-view.container'))
+  }, 'profile')
+
+// import('../../containers/robot-profile-view.container')
 
 function App() {
   return (
@@ -10,7 +18,11 @@ function App() {
       <h1>RoboDex</h1>
       <Switch>
         <Route path="/" exact component={RobotFilterViewContainer} />
-        <Route path="/profile/:id" component={RobotProfileViewContainer} />
+        <Route
+          path="/profile/:id"
+          render={props => <Lazy {...props} load={loadProfile} />}
+        />
+        {/* <Route path="/profile/:id" component={RobotProfileViewContainer} />*/}
         <Route render={() => <Redirect to={{ pathname: '/' }} />} />
       </Switch>
     </div>
